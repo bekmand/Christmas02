8b52645b80dea231f8278db1d02f1a4df81e3f2b
Casper Bekmand <casper.bekmand@hotmail.com>
2015-12-02 15:58:40 +0100

    Christmas02

---
+++ Christmas-02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Christmas_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] text = System.IO.File.ReadAllLines(@"C:\Users\Public\Documents\ChristmasWrap.txt");
+
+            foreach(string line in text)
+            {
+                int lengthStart = line.IndexOf("x");
+                int lengthEnd = line.LastIndexOf("x");
+
+                if(lengthStart == 1)
+                {
+                    
+                }
+
+                string l = line.Substring(0, 1);
+                string w = line.Substring(3, 4);
+                string h = line.Substring(6, 7);
+
+            }
+        }
+    }
+}
